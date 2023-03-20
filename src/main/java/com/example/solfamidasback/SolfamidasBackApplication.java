2b4aab8b74f8035d1e4fa9d8f6580dded1d96e6a
Ruben <rnieto@safareyes.es>
2023-03-20 19:01:23 +0100

    Creacion de proyecto

---
+++ src/main/java/com/example/solfamidasback/SolfamidasBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.solfamidasback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolfamidasBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolfamidasBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/solfamidasback/SolfamidasBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solfamidasback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolfamidasBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
