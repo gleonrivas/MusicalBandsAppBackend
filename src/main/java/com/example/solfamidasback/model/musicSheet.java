f1821e6e5d09de57217605142bbfbf8542586691
lortegaperal <lortegaperal@safareyes.es>
2023-03-23 19:34:32 +0100

    creacion de login y partitura

--- src/main/java/com/example/solfamidasback/model/musicSheet.java
+++ src/main/java/com/example/solfamidasback/model/musicSheet.java
@@ -1,6 +1,5 @@
 package com.example.solfamidasback.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -8,29 +7,36 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
-
 @Entity
-@Table(name = "login")
+@Table(name = "music_sheet")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Login {
+public class musicSheet {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", length = 10)
     private Integer id;
-    @Column(name = "token", nullable = false, length = 600)
-    private String token;
-
-    @Column(name = "expiration_date", nullable = false)
-    private LocalDate expirationDate;
     @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_user")
     private User user;
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "id_formation")
+    private Formation formation;
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "id_role")
+    private Role role;
+    @Column(name = "ms_pdf", nullable = false, length = 150)
+    private String musicSheetPdf;
+
+    @Column(name = "instrument_type", nullable = false)
+    private Integer instrumentType;
 
 
 }
