5007c577eeea572836f0fd00f5bdd26cd3a731fe
lortegaperal <lortegaperal@safareyes.es>
2023-03-24 19:12:21 +0100

    Services creados

--- src/main/java/com/example/solfamidasback/repository/CalendarEventRepository.java
+++ src/main/java/com/example/solfamidasback/repository/CalendarEventRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CalendarRepository extends JpaRepository<Calendar,Integer> {
+public interface CalendarEventRepository extends JpaRepository<Calendar,Integer> {
 }
