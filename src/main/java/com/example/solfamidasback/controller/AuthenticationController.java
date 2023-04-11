bcf6f930acd636111cd715a3c38597fcdc4c0d04
corsev <acornejolopez@safareyes.es>
2023-04-11 19:16:24 +0200

    cambios en validacion login para juanma

--- src/main/java/com/example/solfamidasback/controller/AuthenticationController.java
+++ src/main/java/com/example/solfamidasback/controller/AuthenticationController.java
@@ -3,6 +3,7 @@ package com.example.solfamidasback.controller;
 import com.example.solfamidasback.configSecurity.AuthenticationRequests;
 import com.example.solfamidasback.configSecurity.AuthenticationResponses;
 import com.example.solfamidasback.configSecurity.RegisterRequest;
+import com.example.solfamidasback.controller.DTO.LoginDTO;
 import com.example.solfamidasback.repository.UserRepository;
 import com.example.solfamidasback.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/login")
@@ -71,39 +75,48 @@ public class AuthenticationController {
         var users = userRepository.findByEmail(request.getEmail());
 
         if (!validPassword(request.getPassword())){
-            String mensaje = "La contraseña tiene que tener al menos 8 caracteres y contener al menos un dígito, una letra minúscula y una letra mayúscula";
+            LoginDTO loginDTO = new LoginDTO();
+            loginDTO.setToken(0);
+            loginDTO.setError("La contraseña tiene que tener al menos 8 caracteres y contener al menos un dígito, una letra minúscula y una letra mayúscula");
             HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.TEXT_PLAIN);
-            return new ResponseEntity(mensaje, headers, HttpStatus.BAD_REQUEST);
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            return new ResponseEntity(loginDTO, headers, HttpStatus.BAD_REQUEST);
         }
 
         if (!request.getEmail().contains("@") || !request.getEmail().contains(".")){
-            String mensaje = "Formato de email no válido";
+            LoginDTO loginDTO = new LoginDTO();
+            loginDTO.setToken(0);
+            loginDTO.setError("Formato de email no válido");
             HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.TEXT_PLAIN);
-            return new ResponseEntity(mensaje, headers, HttpStatus.BAD_REQUEST);
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            return new ResponseEntity(loginDTO, headers, HttpStatus.BAD_REQUEST);
         }
 
         if (request.getEmail().isBlank() || request.getPassword().isBlank()) {
-            String mensaje = "Campos mal puestos o email no válido";
+            LoginDTO loginDTO = new LoginDTO();
+            loginDTO.setToken(0);
+            loginDTO.setError("Campos mal puestos o email no válido");
             HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.TEXT_PLAIN);
-            return new ResponseEntity(mensaje, headers, HttpStatus.BAD_REQUEST);
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            return new ResponseEntity(loginDTO, headers, HttpStatus.BAD_REQUEST);
         }
+
         if (!userRepository.existsByEmail(request.getEmail())) {
-            String mensaje = "El email no existe";
+            LoginDTO loginDTO = new LoginDTO();
+            loginDTO.setToken(0);
+            loginDTO.setError("El email no existe");
             HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.TEXT_PLAIN);
-            return new ResponseEntity(mensaje, headers, HttpStatus.BAD_REQUEST);
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            return new ResponseEntity(loginDTO, headers, HttpStatus.BAD_REQUEST);
         }
-
-
 
         if (!encoder.matches(request.getPassword(), users.get().getPassword())){
-            String mensaje = "La contraseña es incorrecta para el email usado";
+            LoginDTO loginDTO = new LoginDTO();
+            loginDTO.setToken(0);
+            loginDTO.setError("La contraseña es incorrecta para el email usado");
             HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.TEXT_PLAIN);
-            return new ResponseEntity(mensaje, headers, HttpStatus.BAD_REQUEST);
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            return new ResponseEntity(loginDTO, headers, HttpStatus.BAD_REQUEST);
 
         }
 
---
+++ src/main/java/com/example/solfamidasback/controller/DTO/LoginDTO.java
@@ -0,0 +1,12 @@
+package com.example.solfamidasback.controller.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginDTO {
+    private Integer token;
+    private String error;
+
+}
