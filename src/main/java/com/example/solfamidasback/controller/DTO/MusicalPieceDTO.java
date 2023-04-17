0fc55959831c0a5340a7ccdf508fa2bfcc722726
celim <celimva@gmail.com>
2023-04-17 18:32:59 +0200

    create musical piece hecho

---
+++ src/main/java/com/example/solfamidasback/controller/DTO/MusicalPieceDTO.java
@@ -0,0 +1,12 @@
+package com.example.solfamidasback.controller.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MusicalPieceDTO {
+    private String name;
+    private String author;
+    private Double length;
+}
--- src/main/java/com/example/solfamidasback/controller/MusicalPieceController.java
+++ src/main/java/com/example/solfamidasback/controller/MusicalPieceController.java
@@ -1,7 +1,8 @@
 package com.example.solfamidasback.controller;
 
+import com.example.solfamidasback.controller.DTO.FormationDTO;
+import com.example.solfamidasback.model.*;
 import com.example.solfamidasback.model.DTO.MusicalPieceDTO;
-import com.example.solfamidasback.model.MusicalPiece;
 import com.example.solfamidasback.repository.MusicalPieceRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -15,11 +16,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -80,7 +79,7 @@ public class MusicalPieceController {
             @ApiResponse(responseCode = "200",content = {@Content(schema = @Schema(implementation = MusicalPiece.class),mediaType = "application/json")}),
             @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
     })
-    @GetMapping("/name/{name}")
+    @GetMapping("/{name}")
     public ResponseEntity<List<MusicalPieceDTO>> musicalPieceActiveByName(@PathVariable String name) {
 
         Set<MusicalPiece> musicalPieceSet = new HashSet<>(musicalPieceRepository.getByNameLikeAndActiveTrue(name));
@@ -93,6 +92,47 @@ public class MusicalPieceController {
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         return new ResponseEntity(musicalPieceDTOList,httpHeaders, HttpStatus.OK);
 
+    }
+
+    @Operation(summary = "Retrieve a musical Piece by author",
+            description = "The response is a list of Musical Piece",
+            tags = {"author"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",content = {@Content(schema = @Schema(implementation = MusicalPiece.class),mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", content = {@Content(schema = @Schema()) }),
+    })
+    @GetMapping("/{author}")
+    public ResponseEntity<List<MusicalPieceDTO>> musicalPieceActiveByAuthor(@PathVariable String author) {
+
+        Set<MusicalPiece> musicalPieceSet = new HashSet<>(musicalPieceRepository.findAllByAuthorAndActiveIsTrue(author));
+        List<MusicalPiece> musicalPieceList = new ArrayList<>(musicalPieceSet);
+        List<MusicalPieceDTO> musicalPieceDTOList = new ArrayList<>();
+        for(MusicalPiece musicalPiece: musicalPieceList){
+            musicalPieceDTOList.add(new MusicalPieceDTO(musicalPiece.getName(),musicalPiece.getAuthor(),musicalPiece.getLength()));
+        }
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return new ResponseEntity(musicalPieceDTOList,httpHeaders, HttpStatus.OK);
+
+    }
+    @Operation(summary = "Create a musical piece",
+            description = "Create a musical piece",
+            tags = {"name","author","length"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+    })
+    @PostMapping("/create")
+    public ResponseEntity<MusicalPiece> createMusicalPiece(@RequestBody com.example.solfamidasback.controller.DTO.MusicalPieceDTO musicalPieceDTO) {
+        MusicalPiece musicalPiece = new MusicalPiece();
+        musicalPiece.setName(musicalPieceDTO.getName());
+        musicalPiece.setAuthor(musicalPieceDTO.getAuthor());
+        musicalPiece.setLength(musicalPieceDTO.getLength());
+        musicalPiece.setActive(true);
+        musicalPieceRepository.save(musicalPiece);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new ResponseEntity(musicalPiece,headers, HttpStatus.OK);
     }
 
 }
