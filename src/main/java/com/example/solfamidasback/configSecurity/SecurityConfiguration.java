df086e1ac01347c83aa6399a0eab59afa5ceac89
celim <celimva@gmail.com>
2023-04-19 21:13:06 +0200

    crud Repertory terminado

--- src/main/java/com/example/solfamidasback/configSecurity/SecurityConfiguration.java
+++ src/main/java/com/example/solfamidasback/configSecurity/SecurityConfiguration.java
@@ -24,7 +24,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/login/**","/v3/**","/swagger-ui/**","/**","/repertory/**")
+                .requestMatchers("/login/**","/v3/**","/swagger-ui/**","/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
