a6dc5ffe928456d2e819f0bc4835568f267537ff
Cornejo <acornejolopez@safareyes.es>
2023-05-02 20:35:01 +0200

    crear material prestado con validaciones

--- src/main/java/com/example/solfamidasback/controller/MaterialController.java
+++ src/main/java/com/example/solfamidasback/controller/MaterialController.java
@@ -1,7 +1,48 @@
 package com.example.solfamidasback.controller;
 
-import org.springframework.stereotype.Controller;
+import com.example.solfamidasback.controller.DTO.BorrowedMaterialDTO;
+import com.example.solfamidasback.model.Material;
+import com.example.solfamidasback.model.Users;
+import com.example.solfamidasback.repository.MaterialRepository;
+import com.example.solfamidasback.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/material")
 public class MaterialController {
+    @Autowired
+    MaterialRepository materialRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("/createBorrowedMaterial")
+    public ResponseEntity<String> saveBorrowedMaterial(@RequestBody BorrowedMaterialDTO borrowedMaterialDTO){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        List<Integer> bm = materialRepository.findBorrowedMaterial(borrowedMaterialDTO.getMaterialId(), borrowedMaterialDTO.getUserId());
+        Optional<Material> material = materialRepository.findById(borrowedMaterialDTO.getMaterialId());
+        Users users = userRepository.findByIdAndActiveIsTrue(borrowedMaterialDTO.getUserId());
+
+        if (material == null || users == null){
+            return new ResponseEntity("material or user not exists or error",headers, HttpStatus.OK);
+        }
+        if (bm.isEmpty()){
+            materialRepository.createBorrowedMaterial(borrowedMaterialDTO.getMaterialId(), borrowedMaterialDTO.getUserId());
+            return new ResponseEntity("created borrowed material",headers, HttpStatus.OK);
+        }
+        return new ResponseEntity("borrowed material it already exists or error",headers, HttpStatus.OK);
+    }
+
 }
+
--- src/main/java/com/example/solfamidasback/repository/MaterialRepository.java
+++ src/main/java/com/example/solfamidasback/repository/MaterialRepository.java
@@ -1,9 +1,25 @@
-//package com.example.solfamidasback.repository;
-//
-//import com.example.solfamidasback.model.Material;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface MaterialRepository extends JpaRepository<Material,Integer> {
-//}
+package com.example.solfamidasback.repository;
+import com.example.solfamidasback.controller.DTO.BorrowedMaterialDTO;
+import com.example.solfamidasback.model.Material;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+public interface MaterialRepository extends JpaRepository<Material,Integer> {
+
+    @Modifying
+    @Query(value = "insert into borrowed_material (id_material, id_users) values (?,?)", nativeQuery = true)
+    @Transactional
+    void createBorrowedMaterial(@Param("id_material")Integer materialId,@Param("id_user")Integer userId );
+
+    @Query(value = "select id_material, id_users from borrowed_material where id_material = ? and id_users = ?;", nativeQuery = true)
+    List<Integer> findBorrowedMaterial(@Param("id_material")Integer materialId, @Param("id_user")Integer userId );
+
+
+}
