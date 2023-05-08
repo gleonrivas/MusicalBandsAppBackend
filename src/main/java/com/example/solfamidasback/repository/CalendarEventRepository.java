3044fd9c80a88b4d63556983551c0e25ffe21a4d
celim <celimva@gmail.com>
2023-05-08 20:12:25 +0200

    crud musico externo terminado

--- src/main/java/com/example/solfamidasback/repository/CalendarEventRepository.java
+++ src/main/java/com/example/solfamidasback/repository/CalendarEventRepository.java
@@ -4,6 +4,10 @@ import com.example.solfamidasback.model.CalendarEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface CalendarEventRepository extends JpaRepository<CalendarEvent,Integer> {
+
+    CalendarEvent findFirstById(Integer id);
+
 }
--- src/main/java/com/example/solfamidasback/repository/ExternalMusicianRepository.java
+++ src/main/java/com/example/solfamidasback/repository/ExternalMusicianRepository.java
@@ -1,7 +1,11 @@
 package com.example.solfamidasback.repository;
 
+import com.example.solfamidasback.model.CalendarEvent;
 import com.example.solfamidasback.model.ExternalMusician;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +13,10 @@ import java.util.List;
 @Repository
 public interface ExternalMusicianRepository extends JpaRepository<ExternalMusician,Integer> {
 
-    public List<ExternalMusician> findAllByActiveIsTrueAndCalendar(Integer idCalendar);
+    public List<ExternalMusician> findAllByActiveIsTrueAndCalendar(CalendarEvent calendarEvent);
 
-    public List<ExternalMusician> findAllByNameAndActiveIsTrue(String name);
+    @Query(value = "select * from external_musician where name like ? and active =true", nativeQuery = true)
+    public List<ExternalMusician> getByNameAndActiveIsTrue(@Param("name")String name);
+
+    ExternalMusician findFirstByIdAndActiveIsTrue(Integer id);
 }
