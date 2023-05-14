ee9340db7e30a89fac3f8988e2c8d3dad0384ff9
rubni <rnietourena@safareyes.es>
2023-05-14 19:55:03 +0200

    empezando absence controller

--- src/main/java/com/example/solfamidasback/controller/AbsenceController.java
+++ src/main/java/com/example/solfamidasback/controller/AbsenceController.java
@@ -1,5 +1,7 @@
 package com.example.solfamidasback.controller;
 
+import com.example.solfamidasback.controller.DTO.FormationIdDTO;
+import com.example.solfamidasback.controller.DTO.RegisterAbsenceDTO;
 import com.example.solfamidasback.controller.DTO.ResponseStringDTO;
 import com.example.solfamidasback.model.CalendarEvent;
 import com.example.solfamidasback.model.Users;
@@ -22,10 +24,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -35,7 +34,7 @@ import java.util.stream.Collectors;
 
 @Tag(name = "Absence", description = "Absence crud")
 @RestController
-@RequestMapping("/calendar")
+@RequestMapping("/absence")
 public class AbsenceController {
     @Autowired
     UserRepository userRepository;
@@ -43,11 +42,51 @@ public class AbsenceController {
     @Autowired
     CalendarEventRepository calendarEventRepository;
 
+    @Autowired CalendarEventService calendarEventService;
+
     private final String HEADER = "Authorization";
     private final String PREFIX = "Bearer ";
     @Autowired
     private JwtService jwtService;
 
+    @Operation(summary = "Retrieve a list of calendar events for the user",
+            description = "The response is a list of Calendar Event Objects",
+            security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",content = {@Content(array = @ArraySchema(schema = @Schema(implementation = CalendarEvent.class)),mediaType = "application/json")}),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema(implementation = String.class)) }),
+    })
+    @PostMapping("RegisterAbsence")
+    public ResponseEntity<CalendarEvent> listAllMyEvents(@RequestBody RegisterAbsenceDTO registerAbsenceDTO, HttpServletRequest request) {
 
+
+        //token validation
+        try {
+            String jwtToken = request.getHeader(HEADER).replace(PREFIX, "");
+        } catch (Exception e) {
+            ResponseStringDTO responseStringDTO = new ResponseStringDTO("Token error");
+            return new ResponseEntity(responseStringDTO, HttpStatus.BAD_REQUEST);
+        }
+
+        String jwtToken = request.getHeader(HEADER).replace(PREFIX, "");
+        String mail = jwtService.extractUsername(jwtToken);
+        Users user = userRepository.findByEmailAndActiveTrue(mail);
+
+        // validacion de datos del body
+
+
+        //validacion de que el usuario pertenece a la formacion
+        CalendarEvent calendarEvent = calendarEventRepository.getReferenceById(Integer.parseInt(registerAbsenceDTO.getCalendarEventId()));
+        if(!user.getFormationList().contains(calendarEvent.getFormation())){
+            ResponseStringDTO responseStringDTO = new ResponseStringDTO("It's not your formation");
+            return new ResponseEntity(responseStringDTO , HttpStatus.BAD_REQUEST );
+        }
+
+
+
+
+
+        return null;
+    }
 
 }
