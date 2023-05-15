20a21c18f39f6bff5703c266d498934ef9090b6e
rubni <rnietourena@safareyes.es>
2023-05-15 18:04:16 +0200

    empezando absence controller

--- src/main/java/com/example/solfamidasback/controller/AbsenceController.java
+++ src/main/java/com/example/solfamidasback/controller/AbsenceController.java
@@ -3,9 +3,14 @@ package com.example.solfamidasback.controller;
 import com.example.solfamidasback.controller.DTO.FormationIdDTO;
 import com.example.solfamidasback.controller.DTO.RegisterAbsenceDTO;
 import com.example.solfamidasback.controller.DTO.ResponseStringDTO;
+import com.example.solfamidasback.model.Absence;
 import com.example.solfamidasback.model.CalendarEvent;
+import com.example.solfamidasback.model.Enums.EnumRolUserFormation;
+import com.example.solfamidasback.model.UserFormationRole;
 import com.example.solfamidasback.model.Users;
+import com.example.solfamidasback.repository.AbsenceRepository;
 import com.example.solfamidasback.repository.CalendarEventRepository;
+import com.example.solfamidasback.repository.UserFormationRoleRepository;
 import com.example.solfamidasback.repository.UserRepository;
 import com.example.solfamidasback.service.CalendarEventService;
 import com.example.solfamidasback.service.JwtService;
@@ -27,6 +32,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -36,13 +42,15 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/absence")
 public class AbsenceController {
-    @Autowired
-    UserRepository userRepository;
+    @Autowired UserRepository userRepository;
 
-    @Autowired
-    CalendarEventRepository calendarEventRepository;
+    @Autowired CalendarEventRepository calendarEventRepository;
 
     @Autowired CalendarEventService calendarEventService;
+
+    @Autowired UserFormationRoleRepository userFormationRoleRepository;
+
+    @Autowired AbsenceRepository absenceRepository;
 
     private final String HEADER = "Authorization";
     private final String PREFIX = "Bearer ";
@@ -74,13 +82,57 @@ public class AbsenceController {
 
         // validacion de datos del body
 
+        if(!calendarEventService.verifyInteger(registerAbsenceDTO.getCalendarEventId())){
+            ResponseStringDTO responseStringDTO = new ResponseStringDTO("Data error");
+            return new ResponseEntity(responseStringDTO, HttpStatus.BAD_REQUEST);
+        }
+        for (String s:registerAbsenceDTO.getListOfUserId()){
+            if(!calendarEventService.verifyInteger(s)){
+                ResponseStringDTO responseStringDTO = new ResponseStringDTO("Data error");
+                return new ResponseEntity(responseStringDTO, HttpStatus.BAD_REQUEST);
+            }
+        }
+        // validacion de que el usuario que registra pertenece a la formacion del evento y su rol es correcto
 
-        //validacion de que el usuario pertenece a la formacion
         CalendarEvent calendarEvent = calendarEventRepository.getReferenceById(Integer.parseInt(registerAbsenceDTO.getCalendarEventId()));
+
         if(!user.getFormationList().contains(calendarEvent.getFormation())){
-            ResponseStringDTO responseStringDTO = new ResponseStringDTO("It's not your formation");
+            ResponseStringDTO responseStringDTO = new ResponseStringDTO("You do not belong to that formation");
+            return new ResponseEntity(responseStringDTO, HttpStatus.BAD_REQUEST);
+        }
+        List<UserFormationRole> formationRoleList = user.getUserFormationRole().stream().filter(userFormationRole ->
+                        userFormationRole.getFormation().getId()==calendarEvent.getFormation().getId())
+                .collect(Collectors.toList()).stream().filter(userFormationRole
+                        -> userFormationRole.getRole().getType().equals(EnumRolUserFormation.OWNER)||
+                        userFormationRole.getRole().getType().equals(EnumRolUserFormation.ASSISTANCE_CONTROL)).collect(Collectors.toList());
+
+        if (formationRoleList.isEmpty()){
+            ResponseStringDTO responseStringDTO = new ResponseStringDTO("You can't register absences");
             return new ResponseEntity(responseStringDTO , HttpStatus.BAD_REQUEST );
         }
+
+        //validar que los usuarios introducidos pertenecen a la formacion
+        for (String s: registerAbsenceDTO.getListOfUserId()){
+            Users  u = userRepository.getById(Integer.parseInt(s));
+            List<UserFormationRole> userFormationRoles = u.getUserFormationRole().stream().filter(userFormationRole ->
+                    userFormationRole.getFormation().equals(calendarEvent.getFormation())).collect(Collectors.toList());
+
+            if(userFormationRoles.size()!=registerAbsenceDTO.getListOfUserId().size()){
+                ResponseStringDTO responseStringDTO = new ResponseStringDTO("There is a user who does not belong to the formation");
+                return new ResponseEntity(responseStringDTO , HttpStatus.BAD_REQUEST );
+            }
+        }
+
+        //guardamos los registros en bbdd
+
+        for(String s:registerAbsenceDTO.getListOfUserId()){
+            Absence absence = new Absence();
+            absence.setCalendar(calendarEvent);
+            absence.setFullDate(LocalDateTime.now());
+            absence.setUsers(userRepository.getReferenceById(Integer.parseInt(s)));
+            absenceRepository.save(absence);
+        }
+
 
 
 
