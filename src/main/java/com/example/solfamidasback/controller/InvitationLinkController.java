58b90be3817262284c568d77cd8eef113bb9f203
Cornejo <acornejolopez@safareyes.es>
2023-05-24 20:34:57 +0200

    metodos creado para juanma borrowed material

--- src/main/java/com/example/solfamidasback/controller/InvitationLinkController.java
+++ src/main/java/com/example/solfamidasback/controller/InvitationLinkController.java
@@ -50,7 +50,7 @@ public class InvitationLinkController {
             @ApiResponse(responseCode = "200",content = {@Content(schema = @Schema(implementation = InvitationLinkDTO.class),mediaType = "application/json")}),
             @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
     })
-    @PatchMapping("/{idFormation}")
+    @DeleteMapping("/{idFormation}")
     public ResponseEntity<InvitationLinkDTO> deleteLink(@NotNull @PathVariable Integer idFormation) {
 
         String response = invitationLinkService.deleteInvitation(idFormation);
